3fe7898cd455b1dc94fe62be9f05423062b04574
AthlonDev <athlons91@gmail.com>
2022-03-09 10:20:35 +0000

    added some responsiveness and logo design

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Navigation Bar Animation
 window.onscroll = function() {scrolling()};
 const nav = document.getElementById("nav")
+const logo = document.getElementById("logo")
 
 
 function scrolling() {
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
        nav.style.padding = "2px 2%";
+
     } 
     else{
         nav.style.padding = "10px 2%";
