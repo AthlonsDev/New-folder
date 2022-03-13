ea4db5ae38caa0ce7e1534b9b2118a4bafb05e29
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-03-13 16:26:58 +0000

    Redesign slideshow homepage

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function scrolling() {
 
 // Slideshow Controller
 // Shows slides with timer
-var sliderIndex = 0;
-showSlides();
+// var sliderIndex = 0;
+// showSlides();
 
 
 
@@ -36,47 +36,38 @@ showSlides();
 //     setTimeout(showSlides, 3000);
 // }
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    sliderIndex++;
-    if (sliderIndex > slides.length) {
-        sliderIndex = 1
-    }
-    slides[sliderIndex - 1].style.display = "block"
-    setTimeout(showSlides, 3000);
-}
-/*
-// Slideshow user controlled
-// var slideIndex = 1;
-
-// showSlides(slideIndex);
-
-// // Next and Previous Controls
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
-
-// // Thumbnail Image COntrols
-// function currentSilde(n) {
-//     showSlides(slideIndex += n)
-// }
-
-// function showSlides(n) {
+// function showSlides() {
 //     var i;
 //     var slides = document.getElementsByClassName("slides");
-//     var dots = document.getElementsByClassName("dots");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
 //     for (i = 0; i < slides.length; i++) {
 //         slides[i].style.display = "none";
 //     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
+//     sliderIndex++;
+//     if (sliderIndex > slides.length) {
+//         sliderIndex = 1
 //     }
-//     slides[slideIndex-1].style.display = "block";
-//     dots[slideIndex-1].className += " active";
-//   }
+//     slides[sliderIndex - 1].style.display = "block"
+//     setTimeout(showSlides, 3000);
+// }
+
+
+
+//Slideshow user controlled
+const buttons = document.querySelectorAll("[dataButton]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[dataContainer]")
+      .querySelector("[dataSlides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
