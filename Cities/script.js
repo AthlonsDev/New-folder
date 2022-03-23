b6b74ea5ad7652f427800c1186069e497c1e03ca
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-03-23 18:29:11 +0000

    added DOM interface

--- Cities/script.js
+++ Cities/script.js
@@ -1,4 +1,41 @@
 
+var cover = document.getElementById("cover");
+var pageTitle = document.getElementById("title");
+var bookBtn = document.getElementById("book");
+var historyImg = document.getElementById("historyImg");
+
+if (document.title == "Cagliari") {
+    
+     cover.style.backgroundImage = "url('../images/Cagliari_2.jpg')";
+
+}
+if (document.title == "Alghero") {
+    
+    cover.style.backgroundImage = "url('../images/Alghero_2.jpg')";
+
+}
+if (document.title == "Nuoro") {
+    
+    cover.style.backgroundImage = "url('../images/Nuoro_1.jpg')";
+    historyImg.style.marginTop = "-30%";
+    bookBtn.style.marginLeft = "44%";
+
+
+}
+if (document.title == "Maddalena") {
+    
+    cover.style.backgroundImage = "url('../images/Maddalena_1.jpg')";
+    title.style.marginLeft = "30%";
+    bookBtn.style.marginLeft = "40%";
+    
+
+
+
+}
+
+
+
+
 function showMoreText() {
     var dots = document.getElementById("dots");
     var more = document.getElementById("more");
